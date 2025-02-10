9657a5384183feb2b0955857f277347d4920c584
Mikhail Kapytok <mishakopytok@gmail.com>
2025-02-10 16:29:04 +0300

    rest get candles

--- TradeApp/Clients/ClientRestApi.cs
+++ TradeApp/Clients/ClientRestApi.cs
@@ -1,15 +1,79 @@
 ﻿using System.ComponentModel;
+using System.Diagnostics;
 using System.Text.Json;
 using System.Text.Json.Nodes;
+using System.Web;
 using TradeApp.Core;
+using static System.Runtime.InteropServices.JavaScript.JSType;
 
 namespace TradeApp.Clients
 {
     public class ClientRestApi : ITestConnectorRest
     {
-        public Task<IEnumerable<Candle>> GetCandleSeriesAsync(string pair, int periodInSec, DateTimeOffset? from, DateTimeOffset? to = null, long? count = 0)
+        public async Task<IEnumerable<Candle>> GetCandleSeriesAsync(string pair, 
+            int periodInSec, DateTimeOffset? from, DateTimeOffset? to = null, 
+            long? count = 0)
         {
-            throw new NotImplementedException();
+            var httpClient = new HttpClient();
+            string timeFrame = ConvertTime(periodInSec);
+
+            string candleQuerry = $"trade:{timeFrame}:{pair}";
+            Console.WriteLine($"Pair ---> {pair}");
+            Console.WriteLine($"CandleQuerry ---> {candleQuerry}");
+
+            UriBuilder uriBuilder = new UriBuilder($"https://api-pub.bitfinex.com/v2/candles/{candleQuerry}/hist");
+            var querryParams = HttpUtility.ParseQueryString(uriBuilder.Query);
+
+            if (from != null)
+            {
+                DateTimeOffset date = (DateTimeOffset)from;
+                querryParams["start"] = Convert.ToString(date.ToUnixTimeMilliseconds());
+            }
+            if (to != null)
+            {
+                DateTimeOffset date = (DateTimeOffset)to;
+                querryParams["end"] = Convert.ToString(date.ToUnixTimeMilliseconds());
+            }
+            if (count != null & count != 0)
+            {
+                querryParams["limit"] = count.ToString();
+            }
+            uriBuilder.Query = querryParams.ToString();
+            string url = uriBuilder.ToString();
+            Console.WriteLine(url);
+
+            var response = await httpClient.GetAsync(url);
+            if (response.IsSuccessStatusCode)
+            {
+                var jsonResp = await response.Content.ReadAsStringAsync();
+                Console.WriteLine(jsonResp);
+                var candles = JsonSerializer.Deserialize<List<List<JsonElement>>>(jsonResp);
+                if (candles == null)
+                {
+                    Console.WriteLine("Error deserialize");
+                    return null;
+                }
+                List<Candle> candleList = new List<Candle>();
+                foreach (var currCandle in candles)
+                {
+                    Candle candle = new Candle()
+                    {
+                        Pair = pair,
+                        OpenPrice = currCandle[1].GetInt64(),
+                        HighPrice = currCandle[3].GetInt64(),
+                        LowPrice = currCandle[4].GetInt64(),
+                        ClosePrice = currCandle[2].GetInt64(),
+                        TotalVolume = currCandle[2].GetDecimal(),
+                        OpenTime = DateTimeOffset.FromUnixTimeMilliseconds(currCandle[0].GetInt64())
+                    };
+                    candleList.Add(candle);
+                }
+                return candleList;
+            }
+            else
+            {
+                return null;
+            }
         }
 
         public async Task<IEnumerable<Trade>> GetNewTradesAsync(string pair, int maxCount)
@@ -48,6 +112,37 @@ namespace TradeApp.Clients
             {
                 return null;
             }
+        }
+
+        private string ConvertTime(int periodInSec)
+        {
+            string timeFrame;
+            if (periodInSec <= 60)
+                timeFrame = "1m";
+            else if (periodInSec <= 60 * 5)
+                timeFrame = "5m";
+            else if (periodInSec <= 60 * 15)
+                timeFrame = "15m";
+            else if (periodInSec <= 60 * 30)
+                timeFrame = "30m";
+            else if (periodInSec <= 60 * 60)
+                timeFrame = "1h";
+            else if (periodInSec <= 60 * 60 * 3)
+                timeFrame = "3h";
+            else if (periodInSec <= 60 * 60 * 6)
+                timeFrame = "6h";
+            else if (periodInSec <= 60 * 60 * 12)
+                timeFrame = "12h";
+            else if (periodInSec <= 60 * 60 * 24)
+                timeFrame = "1D";
+            else if (periodInSec <= 60 * 60 * 24 * 7)
+                timeFrame = "1W";
+            else if (periodInSec <= 60 * 60 * 24 * 14)
+                timeFrame = "14D";
+            else
+                timeFrame = "1M";
+
+            return timeFrame;
         }
     }
 }
--- TradeApp/Controllers/RestController.cs
+++ TradeApp/Controllers/RestController.cs
@@ -10,10 +10,21 @@ namespace TradeApp.Controllers
     {
         private ClientRestApi _restApi = new ClientRestApi();
 
-        [HttpGet("/{pair}/{maxCount:int}")]
+        [HttpGet("Trade/{pair}/{maxCount:int}")]
         public async Task<ActionResult<List<Trade>>> GetTrades(string pair, int maxCount)
         {
             var resp = await _restApi.GetNewTradesAsync(pair, maxCount);
+            if (resp == null)
+                return NotFound();
+            return Ok(resp);
+        }
+
+        [HttpGet("Candle/{pair}")]
+        public async Task<ActionResult<List<Candle>>> GetCandles(string pair, 
+            [FromQuery] int periodInSec, [FromQuery] DateTimeOffset? from = null, 
+            [FromQuery] DateTimeOffset? to = null, [FromQuery]long? count = 0)
+        {
+            var resp = await _restApi.GetCandleSeriesAsync(pair, periodInSec, from, to, count);
             if (resp == null)
                 return NotFound();
             return Ok(resp);
