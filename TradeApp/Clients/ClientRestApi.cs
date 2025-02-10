be3fb6f9a046ae92cd6a2291b9cbcb9bf6cc4056
Mikhail Kapytok <mishakopytok@gmail.com>
2025-02-10 15:14:16 +0300

    rest get trades

--- TradeApp/Clients/ClientRestApi.cs
+++ TradeApp/Clients/ClientRestApi.cs
@@ -1,4 +1,7 @@
-﻿using TradeApp.Core;
+﻿using System.ComponentModel;
+using System.Text.Json;
+using System.Text.Json.Nodes;
+using TradeApp.Core;
 
 namespace TradeApp.Clients
 {
@@ -9,9 +12,42 @@ namespace TradeApp.Clients
             throw new NotImplementedException();
         }
 
-        public Task<IEnumerable<Trade>> GetNewTradesAsync(string pair, int maxCount)
+        public async Task<IEnumerable<Trade>> GetNewTradesAsync(string pair, int maxCount)
         {
-            throw new NotImplementedException();
+            var httpClient = new HttpClient();
+            string url = $"https://api-pub.bitfinex.com/v2/trades/{pair}/hist?limit={maxCount}";
+            var response = await httpClient.GetAsync(url);
+            if (response.IsSuccessStatusCode)
+            {
+                var jsonResp = await response.Content.ReadAsStringAsync();
+                Console.WriteLine(jsonResp);
+                var trades = JsonSerializer.Deserialize<List<List<JsonElement>>>(jsonResp);
+                if (trades == null)
+                {
+                    Console.WriteLine("Error deserialize");
+                    return null;
+                }
+
+                List<Trade> tradeList = new List<Trade>();
+                foreach (var currTrade in trades)
+                {
+                    Trade trade = new Trade()
+                    {
+                        Pair = pair,
+                        Id = Convert.ToString(currTrade[0].GetInt64()),
+                        Time = DateTimeOffset.FromUnixTimeMilliseconds(currTrade[1].GetInt64()),
+                        Amount = Math.Abs(currTrade[2].GetDecimal()),
+                        Price = currTrade[3].GetDecimal(),
+                        Side = currTrade[2].GetDecimal() > 0 ? "buy" : "sell"
+                    };
+                    tradeList.Add(trade);
+                }
+                return tradeList;
+            }
+            else
+            {
+                return null;
+            }
         }
     }
 }
---
+++ TradeApp/Controllers/RestController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using TradeApp.Clients;
+using TradeApp.Core;
+
+namespace TradeApp.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class RestController : Controller
+    {
+        private ClientRestApi _restApi = new ClientRestApi();
+
+        [HttpGet("/{pair}/{maxCount:int}")]
+        public async Task<ActionResult<List<Trade>>> GetTrades(string pair, int maxCount)
+        {
+            var resp = await _restApi.GetNewTradesAsync(pair, maxCount);
+            if (resp == null)
+                return NotFound();
+            return Ok(resp);
+        }
+    }
+}
